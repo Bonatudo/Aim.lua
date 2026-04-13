fb50959f81d923030b9235b0fc79e44f818b3c2b
Bonatudo <bonasties@gmail.com>
2026-04-13 15:28:48 -0400

    Create configuration file for Aimbot and ESP settings

---
+++ Core/Config.lua
@@ -0,0 +1,20 @@
+-- MainV3 Configuration Settings for Aimbot and ESP
+
+AimbotSettings = {
+    AimEnabled = true,
+    AimFOV = 90, -- Field of view for Aimbot
+    AimSmoothness = 5, -- Smoothness of Aimbot
+    TargetPriority = "Nearest", -- Can be "Nearest", "Weakest", etc.
+    AimKey = "MouseButton2", -- Key to enable Aimbot
+}
+
+ESPSettings = {
+    ESPEnabled = true,
+    TeamCheck = true, -- Show ESP for teammates?
+    BoxVisible = true, -- Show box around players?
+    NameVisible = true, -- Show player names?
+    DistanceVisible = true, -- Show distance to player?
+    ESPOffset = Vector3.new(0, 2, 0), -- Offset for ESP
+}
+
+-- Customize the parameters above to fit your needs.
